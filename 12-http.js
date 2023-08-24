7332a5bf11cc29717e37e5c21f11b0d9c78771e2
Christopher Lajoie Asselin <clajoieasselin@gmail.com>
2023-08-24 11:58:44 -0400

    first commit

---
+++ 12-http.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const server = http
+  .createServer((req, res) => {
+    if (req.url === '/') {
+      res.write('Welcome to our home page');
+    } else if (req.url === '/about') {
+      res.write('Here is a short story');
+    } else {
+      res.write(`<h1>Oops!</h1>
+  <p>We can't seem to find the page you are looking for</p>
+  <a href="/">back home</a>
+  `);
+    }
+    res.end();
+  })
+  .listen(3000);
